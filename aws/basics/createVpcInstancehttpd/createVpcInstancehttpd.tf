4966a9771ace9bccb207f4bbaf8434c034557be5
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-12-07 09:27:56 +0530

    Create  createVpcInstancehttpd.tf

---
+++ aws/basics/createVpcInstancehttpd/createVpcInstancehttpd.tf
@@ -0,0 +1,25 @@
+# 1. Create VPC
+
+
+# 2. Create Internet Gateway
+
+
+# 3. Create custom Router table
+
+
+# 4. Create Subnet
+
+
+# 5.Associate subnet with Route table
+
+
+# 6. Create security Group to allow port 22, 80, 443
+
+
+# 7. Create a network interface with the ip in the subnet created in step 4
+
+
+# 8. Assign elastic ip to the interface created in step 7
+
+
+# 9. Create Ubuntu server and install/enable apache2
