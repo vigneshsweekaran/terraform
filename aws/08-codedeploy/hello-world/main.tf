7b1d99760ad20d672bab8e2b86c26b5b62feee9c
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-07-17 11:32:06 +0000

    updated

--- aws/08-codedeploy/hello-world/main.tf
+++ aws/08-codedeploy/hello-world/main.tf
@@ -84,6 +84,26 @@ resource "aws_iam_role" "ec2-role" {
 EOF
 }
 
+resource "aws_iam_role_policy" "hello-world" {
+  role = aws_iam_role.ec2-role.name
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:Get*",
+        "s3:List*"
+      ],
+      "Resource": ["*"]
+    }
+  ]
+}
+POLICY
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_profile"
   role = aws_iam_role.ec2-role.name
