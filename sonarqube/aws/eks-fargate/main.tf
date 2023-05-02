415c75006101b8bb2700752c59087dd5fdcd80ac
vigneshsweekaran <vigneshsweekaran@gmail.com>
2023-05-02 05:57:16 +0000

    Updated the eks

--- sonarqube/aws/eks-fargate/main.tf
+++ sonarqube/aws/eks-fargate/main.tf
@@ -1,23 +1,25 @@
+locals {
+  cluster_name = "sonarqube"
+}
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_id
+data "aws_vpc" "default" {
+  default = true
 }
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_id
-}
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+
+data "aws_subnets" "subnets_id" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_version = "1.24"
-  cluster_name    = "sonarqube"
+  cluster_name    = local.cluster_name
   cluster_endpoint_public_access = true
-  vpc_id          = "vpc-01e1fb54b0dd55f4f"
-  subnet_ids      = ["subnet-054e35f09c0d79dd6", "subnet-00905df181b5372b2", "subnet-065e96d7b556ba5e7"]
+  vpc_id          = data.aws_vpc.default.id
+  subnet_ids      = slice(data.aws_subnets.subnets_id.ids,4,6)
 
   cluster_addons = {
     kube-proxy = {}
