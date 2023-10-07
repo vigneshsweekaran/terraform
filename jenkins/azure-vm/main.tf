986d742a6a4698a9946e0b28af3c73a06c494e23
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-10-07 21:51:26 +0800

    Updated

--- jenkins/azure-vm/main.tf
+++ jenkins/azure-vm/main.tf
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "jenkins" {
 
 resource "azurerm_network_security_group" "jenkins" {
   name                = local.name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.jenkins.location
+  resource_group_name = var.resource_group_name
 
   security_rule {
     name                       = "allow-22-8080"
@@ -45,10 +45,6 @@ resource "azurerm_network_security_group" "jenkins" {
     destination_port_range     = "22,8080"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "Production"
   }
 }
 
