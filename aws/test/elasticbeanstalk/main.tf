40f1799f4b0996c1a1f427e0cc10615bef844ecd
vignesh sweekaran <vigneshsweekaran@gmail.com>
2024-03-22 21:49:27 +0530

    Added test elasticbeanstalk terrform code

---
+++ aws/test/elasticbeanstalk/main.tf
@@ -0,0 +1,115 @@
+provider aws {
+  region = "us-east-1"
+}
+
+locals {
+  name = "demo"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_iam_policy" "elasticbeanstallk_webtier" {
+  name = "AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_role" "instance" {
+  name = "${local.name}-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [data.aws_iam_policy.elasticbeanstallk_webtier.arn]
+}
+
+resource "aws_iam_instance_profile" "instance" {
+  name = "${local.name}-instance-profile"
+  role = aws_iam_role.instance.name
+}
+
+resource "aws_elastic_beanstalk_application" "elasticbeanstalk_app" {
+  name = "${local.name}-app"
+}
+
+resource "aws_elastic_beanstalk_environment" "beanstalkappenv" {
+  name                = "${local.name}-env"
+  application         = aws_elastic_beanstalk_application.elasticbeanstalk_app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.1.0 running PHP 8.1"
+  tier                = "WebServer"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = data.aws_vpc.default.id
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  aws_iam_instance_profile.instance.name
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     =  "True"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", data.aws_subnets.default.ids)
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.medium"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+}
